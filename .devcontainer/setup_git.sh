3fe2a80219ce669ca84dbea21e5abb5b3a3c957f
Adam Krysztopa <krysztopa@gmail.com>
2025-10-18 12:24:13 +0000

    devcontainer

--- .devcontainer/setup_git.sh
+++ .devcontainer/setup_git.sh
@@ -6,6 +6,6 @@ if [ -z "$GIT_USER_NAME" ] || [ -z "$GIT_USER_EMAIL" ]; then
     exit 1
 fi
 
-git config --global user.name "AdamKrysztopa"
-git config --global user.email "krysztopa@gmail.com"
-echo "Git global config updated: "AdamKrysztopa" <krysztopa@gmail.com>"
+git config --global user.name "$GIT_USER_NAME"
+git config --global user.email "$GIT_USER_EMAIL"
+echo "Git global config updated: $GIT_USER_NAME <$GIT_USER_EMAIL>"
